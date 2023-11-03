9e38042d208971eb90bc51fa22d0a0a7d7a6bb77
Sahvasharmid <sahvasharmid@gmail.com>
2023-11-03 22:38:04 +0400

    chang

--- script.js
+++ script.js
@@ -2,19 +2,14 @@ const express=require("express")
 const app=express();
 const bookrouter=require("./routers/bookrouter")
 
-import path from "path";
 const cors=require("cors")
 
 app.use(express.json( { extended: false} ));
 app.use(express.json())
 app.use(cors())
-app.use(express.static(path.join(__dirname,'./frontend/myapp/build')))
+
 app.use("/newbooks",bookrouter)
-app.get("*",function(req,res){
-    res.sendFile(path.join(__dirname,"./frontend/myapp/build/index.html"))
-    })
-  
-  
+
 const PORT=5555||process.env.PORT
 app.listen(PORT,()=>{
     console.log(`port running on ${PORT}`)
